ae27847785ada9341f04c6ab009ce72c44006d66
chisheng <chisheng1686@163.com>
2021-07-20 23:56:17 +0800

    add JZ18

---
+++ JZ18-二叉树的镜像/JZ18.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+    TreeNode* Mirror(TreeNode* pRoot) {
+        if(pRoot == NULL){
+            return NULL;
+        }
+        TreeNode* temp = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = temp;
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+        return pRoot;
+    }
+};
