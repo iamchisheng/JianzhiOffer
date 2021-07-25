9c707d28e5dd09687c45717d3df37c3508f950b8
chisheng <chisheng1686@163.com>
2021-07-26 00:00:27 +0800

    modify JZ34

--- JZ34-第一个只出现一次的字符/JZ34.cpp
+++ JZ34-第一个只出现一次的字符/JZ34.cpp
@@ -2,25 +2,16 @@
 using namespace std;
 class Solution {
 public:
-    int GetUglyNumber_Solution(int index) {
-        if(index < 7){
-            return index;
+    int FirstNotRepeatingChar(string str) {
+        unordered_map<char, int> mp;
+        for(const char ch : str){
+            mp[ch] += 1;
         }
-        vector<int> res(index);
-        res[0] = 1;
-        int t2 = 0, t3 = 0, t5 = 0;
-        for(int i = 1; i < index; i++){
-            res[i] = min(res[t2] * 2, min(res[t3] * 3, res[t5] * 5));
-            if(res[i] == res[t2] * 2){
-                t2++;
-            }
-            if(res[i] == res[t3] * 3){
-                t3++;
-            }
-            if(res[i] == res[t5] * 5){
-                t5++;
+        for(int i = 0; i < str.size(); i++){
+            if(mp[str[i]] == 1){
+                return i;
             }
         }
-        return res[index - 1];
+        return -1;
     }
 };
